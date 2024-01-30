8273aa63719a5ecc5a89b7ec15b9fa092b4170d6
Hammed <hammedtosin188@gmail.com>
2024-01-30 22:06:58 +0000

    making changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -15,8 +15,8 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
+    
+    dp = [float('inf') for _ in range(total + 1)]
     dp[0] = 0
 
     for coin in coins:
